fd72427628f85d3fe951404b48da31c5133a225e
fnzr <5471818+fnzr@users.noreply.github.com>
2022-02-16 19:02:56 -0300

    GraphQL: tentative non-pk query associations

--- GraphQL/Operation/QueryOperation.php
+++ GraphQL/Operation/QueryOperation.php
@@ -38,6 +38,7 @@ class QueryOperation extends AbstractOperation
     public bool $isPrepared = false;
     public bool $isSingleResult = false;
     public bool $includeTypename = false;
+    public Set $requiredSelections;
     public string $typename = '';
     public MModelMaestro|MModel $model;
 
@@ -45,6 +46,7 @@ class QueryOperation extends AbstractOperation
     {
         parent::__construct($context);
         $this->selection = new Set();
+        $this->requiredSelections = new Set();
     }
 
     public static function isAssociationReadable(MModelMaestro|MModel $model, string $name)
@@ -163,6 +165,7 @@ class QueryOperation extends AbstractOperation
         $operation->prepare(Manager::getContainer()->get($associationMap->getToClassName()));
         $name = $node->alias ? $node->alias->value : $node->name->value;
         $this->subOperations[$name] = $operation;
+        $this->requiredSelections->add($associationMap->getFromKey());
     }
 
     /**
@@ -257,20 +260,18 @@ class QueryOperation extends AbstractOperation
         if (!$this->isPrepared) {
             $this->prepare($model);
         }
-        $shouldExcludePK = true;
-        $classMap = $criteria->getClassMap();
-        $pk = $classMap->getKeyAttributeName();
-        if (count($this->subOperations) > 0) {
-            $shouldExcludePK = !$this->selection->contains($pk);
-            $this->selection->add($pk);
+        $columnsToExclude = [];
+        foreach ($this->requiredSelections->toArray() as $item) {
+            if (!$this->selection->contains($item)) {
+                $columnsToExclude[] = $item;
+                $this->selection->add($item);
+            }
         }
+        $classMap = $criteria->getClassMap();
         $criteria->select(join(",", $this->selection->toArray()));
         $this->applyArguments($criteria);
         $rows = $criteria->asResult();
 
-        if (count($this->subOperations) > 0) {
-            $ids = array_map(fn($row) => $row[$pk], $rows);
-        }
         if ($this->includeTypename) {
             foreach ($rows as &$row) {
                 $row['__typename'] = $this->typename;
@@ -278,46 +279,47 @@ class QueryOperation extends AbstractOperation
         }
 
         foreach ($this->subOperations as $associationName => $operation) {
-            if ($associationMap = $classMap->getAssociationMap($associationName)) {
-                $toClassMap = $associationMap->getToClassMap();
-                $subCriteria = $toClassMap->getCriteria();
-                $cardinality = $associationMap->getCardinality();
-                $fk = $classMap->getKeyAttributeName();
+            $associationMap = $classMap->getAssociationMap($associationName);
+            $fk = $associationMap->getFromKey();
+            $keys = array_map(fn($row) => $row[$fk], $rows);
+            $toClassMap = $associationMap->getToClassMap();
+            $subCriteria = $toClassMap->getCriteria();
+            $cardinality = $associationMap->getCardinality();
 
-                $shouldIncludeEmpty = strcasecmp($criteria->getAssociationType($associationName), 'LEFT') == 0;
+            $shouldIncludeEmpty = strcasecmp($criteria->getAssociationType($associationName), 'LEFT') == 0;
+            if ($cardinality == 'oneToOne') {
+                $newAssociation = $this->createTemporaryAssociation($toClassMap, $classMap);
+                $joinField = $newAssociation->getName() . "." . $associationMap->getFromKey();
+                $subCriteria->where($joinField, 'IN', $keys);
+                $subCriteria->select($joinField);
+            } else if ($cardinality == 'manyToOne' || $cardinality == 'oneToMany') {
+                $subCriteria->where($fk, 'IN', $keys);
+                $subCriteria->select($fk);
+            } else {
+                throw new EGraphQLException([$cardinality => 'Unhandled cardinality']);
+            }
+            $subResult = $operation->execute($subCriteria);
+            $subResult = group_by($subResult, $fk, false);
+            $updatedRows = [];
+            foreach ($rows as $row) {
+                $value = $subResult[$row[$fk]] ?? [];
                 if ($cardinality == 'oneToOne') {
-                    $newAssociation = $this->createTemporaryAssociation($toClassMap, $classMap);
-                    $joinField = $newAssociation->getName() . "." . $classMap->getKeyAttributeName();
-                    $subCriteria->where($joinField, 'IN', $ids);
-                    $subCriteria->select($joinField);
-                } else if ($cardinality == 'manyToOne' || $cardinality == 'oneToMany') {
-                    $subCriteria->where($fk, 'IN', $ids);
-                    $subCriteria->select($fk);
-                } else {
-                    throw new EGraphQLException([$cardinality => 'Unhandled cardinality']);
+                    $value = $value[0] ?? null;
                 }
-                $subResult = $operation->execute($subCriteria);
-                $subResult = group_by($subResult, $fk);
-                $updatedRows = [];
-                foreach ($rows as $row) {
-                    $value = $subResult[$row[$pk]] ?? [];
-                    if ($cardinality == 'oneToOne') {
-                        $value = $value[0] ?? null;
-                    }
-                    if ($shouldExcludePK) {
-                        unset($row[$pk]);
-                    }
-                    $row[$associationName] = $value;
-                    if (empty($value)) {
-                        if ($shouldIncludeEmpty) {
-                            $updatedRows[] = $row;
-                        }
-                    } else {
+                foreach ($columnsToExclude as $column) {
+                    unset($row[$column]);
+                }
+                $row[$associationName] = $value;
+                if (empty($value)) {
+                    if ($shouldIncludeEmpty) {
                         $updatedRows[] = $row;
                     }
+                } else {
+                    $updatedRows[] = $row;
                 }
-                $rows = $updatedRows;
             }
+            $rows = $updatedRows;
+
         }
         return ($this->isSingleResult || $this->context->isSingular($this->node->name->value)) ? $rows[0] : $rows;
     }
